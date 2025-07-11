5763e319f73bcd51b81a688a0ec974576b536aad
omarigg04 <gaxiolaomar04@gmail.com>
2025-07-10 18:47:59 -0600

    Enable CORS to allow POST and other HTTP methods when debugging on mobile over Wi-Fi (no USB)

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: '*', // o restringido a IP de tu cel si prefieres
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  });
 }
 bootstrap();
+
+
+
